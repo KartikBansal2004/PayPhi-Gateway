8fee3993703f5b3a3c60b8f2278af1dbeddac9f9
Kartik Bansal <156319570+KartikBansal2004@users.noreply.github.com>
2025-07-28 16:43:50 +0530

    dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "kartik",
-  password: "ppc8r8822",
-  database: "payment_gateway",
+host: "localhost",
+user: "kartik",
+password: "ppc8r8822",
+database: "payment_gateway"
 });
 
 db.connect((err) => {
-  if (err) throw err;
-  console.log("✅ Connected to MySQL database");
+  if (err) {
+    console.error('❌ Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('✅ Connected to MySQL database');
 });
 
 module.exports = db;
