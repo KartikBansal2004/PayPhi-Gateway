a79616773668af29f4ef0951e48e4344b582f732
Kartik Bansal <156319570+KartikBansal2004@users.noreply.github.com>
2025-07-30 14:20:22 +0530

    dbConnection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "kartik",
-password: "ppc8r8822",
-database: "payment_gateway"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 
 db.connect((err) => {
   if (err) {
